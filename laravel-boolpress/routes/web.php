59464214973bf3b4391b7a07bf50eba6cd6ddd06
Emanuele <cannone.emanuele@gmail.com>
2021-03-25 16:56:57 +0100

    creazione rotta home visualizzazione posts

--- laravel-boolpress/routes/web.php
+++ laravel-boolpress/routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('guest-home');
+Route::get('/posts', 'PostController@index')->name('guest.posts.index');
 
 Auth::routes();
 
